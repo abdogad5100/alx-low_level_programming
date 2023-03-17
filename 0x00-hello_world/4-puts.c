5903b87504457bab217cbd18f3166105824ecbd7
abdogad <abdogad963@gmail.com>
2023-03-16 19:47:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,13 @@
 
 /**
  * main - print "Programming is like building a multilingual
- * puzzle, followed by a newline"
+ * puzzle, followed by a newline
  * Return: Always 0.
  */
 
 int main(void)
+
+
 {
 	puts("\"Programming is like building a multilingual puzzle");	
 	return (0);
