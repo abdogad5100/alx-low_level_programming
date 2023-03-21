f0de16d6e1889b13b13802b4f6dc153d7d916922
abdogad <abdogad963@gmail.com>
2023-03-20 19:20:39 -0700

    sd

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,7 +3,6 @@
  * Auth: abdogad
  */
 #include <stdio.h>
-
 /*
  * main - Prints all possible combinations of single-digit numbers.
  *
@@ -18,7 +17,6 @@ int main(void)
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-	
 		putchar(',');
 		putchar(' ');
 	}
